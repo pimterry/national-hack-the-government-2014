50f5bebccc00f52cc91e2704269de6d492e7ed4f
SwamWithTurtles <12e.david@gmail.com>
2014-03-08 20:45:35 +0000

    Add surprise API end point currently stubbed with a constant 0.1

--- stats-api/src/stats_api/routes.clj
+++ stats-api/src/stats_api/routes.clj
@@ -1,6 +1,7 @@
 (ns stats-api.routes
   (:use compojure.core)
   (:use ring.middleware.json)
+  (:use stats-api.statistical-logic.surprise)
   (:require [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [stats-api.data-layer.repository :as fetch]))
@@ -10,6 +11,7 @@
            (GET "/years" [] {:body { :years (fetch/years)}})
            (GET "/areas" [] {:body { :areas (fetch/areas)}})
            (GET "/politicians" [] {:body { :people (fetch/politician)}})
+           (GET "/elections/:year/:area/:politician" [year areaId politicianId] {:body { :surprise (surprise year areaId politicianId)}})
            (route/not-found "Page not found"))
 
 (defn -main [] (jetty/run-jetty (wrap-json-response api-routes) {:port 8080}))
---
+++ stats-api/src/stats_api/statistical_logic/surprise.clj
@@ -0,0 +1,3 @@
+(ns stats-api.statistical-logic.surprise)
+
+(defn surprise [year, areaId, politicianId] 0.1)
