f7bcacb7bc301379701920ceca3143ee7d5ada94
SwamWithTurtles <12e.david@gmail.com>
2014-03-08 20:26:15 +0000

    Add node IDs to areas on API endpoint

--- stats-api/src/stats_api/data_layer/years.clj
+++ stats-api/src/stats_api/data_layer/years.clj
@@ -1,7 +1,4 @@
 (ns stats-api.data-layer.years
-   (:use stats-api.data-layer.connection-config)
+
    )
 
-(defn fetch-years []
-  (run-query "MATCH (y:year) RETURN y.name AS name")
-)
--- stats-api/src/stats_api/repository.clj
+++ stats-api/src/stats_api/repository.clj
@@ -1,7 +1,6 @@
 (ns stats-api.repository
-  (:use stats-api.data-layer.years)
-  (:use stats-api.data-layer.areas))
+  (:use stats-api.data-layer.connection-config))
 
-  (defn years [] [(fetch-years)])
+  (defn years [] (run-query "MATCH (y:year) RETURN y.name AS name"))
 
-  (defn areas [] [(fetch-areas)])
+  (defn areas [] (run-query "MATCH (a:area) RETURN a.name AS name, id(a) AS id"))
