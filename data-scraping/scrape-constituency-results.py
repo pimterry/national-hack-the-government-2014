2dda18cb36c9c7155f329358cf8f6d9f6e2fc523
Tim Perry <pimterry@gmail.com>
2014-03-08 23:12:19 +0000

    Handle many many more inconsistencies in our data source

--- data-scraping/scrape-constituency-results.py
+++ data-scraping/scrape-constituency-results.py
@@ -1,16 +1,31 @@
-import sys, re, urlparse
+import sys, re, urlparse, string
+from itertools import *
 import mechanize, bs4 as BeautifulSoup
 from py2neo import neo4j
 
 def get_all_election_results():
+  have_already_seen_1974 = False
+
   election_urls = get_all_election_page_urls()
   for election_url, election_year in election_urls:
+    # We skip the 1974 February election because it was rubbish
+    if election_year == 1974 and not have_already_seen_1974:
+      have_already_seen_1974 = True
+      continue
+
+    # We have to special case these elections, because the data is *totally* different, sigh
+    if election_year in (1997, 2001):
+      print "Skipping %s for now" % election_year
+      continue
     print "Getting election %s" % election_year
-    
+
     for region_url, region_name in get_all_region_urls(election_url):
       print "Getting region %s" % region_name
       for result in get_all_overall_results(region_url):
         yield result
+
+  for result in get_1997_and_2001_results():
+    yield result
 
 def get_all_election_page_urls():
   browser = mechanize.Browser()
@@ -53,18 +68,46 @@ def get_all_overall_results(page_url):
       results_rows = results_table.findAll("tr")
 
       yield {
-          "area": unicode(area.string),
+          "area": string.capwords(area.string),
           "year": year,
           "results": filter(lambda x: x is not None,
                             [build_result(row) for row in results_rows])
       }
 
+def get_1997_and_2001_results():
+  browser = mechanize.Browser()
+  browser.open("http://www.politicsresources.net/area/uk/mps01.htm")
+  region_urls = [link.absolute_url for link in browser.links()
+                                   if "constit/" in link.url]
+  
+  for url in region_urls:
+    page = browser.open(url)
+    html_soup = BeautifulSoup.BeautifulSoup(page.read())
+    area = string.capwords(unicode(html_soup.find("h1").string))
+    table_titles = html_soup.findAll("h2")
+    
+    for title in table_titles:
+      table = title.findNext("table")
+      if table and "general election" in unicode(title.string).lower():
+        print area
+        print title.string
+        results_rows = table.findAll("tr")
+        yield {
+            "area": area,
+            "year": re.search("(\d\d\d\d)", title.string).groups()[0],
+            "results": filter(lambda x: x is not None,
+                              [build_result(row) for row in results_rows if row.findAll("td")])
+        }
+
+
+
 def build_result(results_row):
-  cell_values = [unicode(cell.string) for cell in results_row.findAll("td")]
+  cell_values = [unicode(cell_content) for cell_content in results_row.strings
+                 if len(cell_content.strip()) > 0]
   try:
     return {
-        "person": cell_values[0],
-        "party": cell_values[1],
+        "person": cell_values[0].strip(),
+        "party": string.capwords(cell_values[1]),
         "votes": int(cell_values[2].replace(",", ""))
     }
   except:
