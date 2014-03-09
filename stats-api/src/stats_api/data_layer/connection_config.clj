6fc481326bf9c5effc87e615348f74f5ebd75ac0
Tim Perry <pimterry@gmail.com>
2014-03-09 12:06:43 +0000

    Pick up GrapheneDB URL from env vars, with localhost default (slightly differently, because for some reason only this works for me)

--- stats-api/src/stats_api/data_layer/connection_config.clj
+++ stats-api/src/stats_api/data_layer/connection_config.clj
@@ -3,7 +3,8 @@
             [clojurewerkz.neocons.rest.cypher :as cy]
             [environ.core :as environ]))
 
-(defn connection-string [] (nr/connect! (environ/env :graphenedb-url)))
+(let [env (get (System/getenv) "GRAPHENEDB_URL" "http://localhost:7474/db/data")]
+  (defn connection-string [] (nr/connect! env)))
 
 (defn run-query [query]
   (connection-string)
