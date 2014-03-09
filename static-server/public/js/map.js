dc506dd19be02211b46b3e8c7f584f6b7fc0c6ee
Tim Perry <pimterry@gmail.com>
2014-03-09 06:17:51 +0000

    Hook up a very quic test of map (fake) data usage

--- static-server/public/js/map.js
+++ static-server/public/js/map.js
@@ -1,19 +1,37 @@
 (function () {
-  svg_height = 400.0
-  var svg = d3.select('.map')
-               .append('svg')
-               .attr('width', 330)
-               .attr('height', svg_height);
+  function draw_map(fill_function) {
+    svg_height = 400.0
+    $(".map").html("")
+    var svg = d3.select('.map')
+                 .append('svg')
+                 .attr('width', 330)
+                 .attr('height', svg_height);
 
-  var map = UK.ElectionMap();
-  map.origin({x: 55, y: 375});
-  map.fill(function (constituency) {
-    return '#'+(0x1000000+(Math.random())*0xffffff)
-            .toString(16).substr(1,6)
+    var map = UK.ElectionMap();
+    map.origin({x: 55, y: 375});
+    map.fill(fill_function);
+    map(svg);
+
+    target_height = $(".map").height() 
+    zoom_percentage = (target_height/svg_height) * 100
+    $(".map svg").css("zoom", zoom_percentage + "%")
+  }
+
+  draw_map(function() { return "#FFFFFF"; });
+
+  dimension_repo.get_areas(function (areas) {
+    party_colours = {
+      "LibDem": "#FDBB30",
+      "Labour": "#DC241f",
+      "Conservative": "#0087DC"
+    };
+
+    draw_map(function (area) {
+      if (areas[area]) {
+        return party_colours[areas[area].party] || "#EEEEEE";
+      } else {
+        return "#EEEEEE";
+      }
+    });
   });
-  map(svg);
-
-  target_height = $(".map").height() 
-  zoom_percentage = (target_height/svg_height) * 100
-  $(".map svg").css("zoom", zoom_percentage + "%")
 })();                 
--- static-server/public/js/surprise-repo.js
+++ static-server/public/js/surprise-repo.js
@@ -1,12 +1,19 @@
 dimension_repo = (function () {
   return {
-    get_areas: function () {
-      return [
+    get_areas: function (callback) {
+      results = [
         {
           "name": "Taunton Deane", 
           "party": "LibDem",     
         }
       ];
+
+      area_map = {}
+      for (var ii = 0; ii < results.length; ii++) {
+        area_map[results[ii].name] = results[ii];
+      }
+
+      callback(area_map);
     },
     get_years: function () {
       return [];
@@ -18,14 +25,14 @@ dimension_repo = (function () {
       return [];
     }
   };
-});
+})();
 
 surprise_repo = (function () {
   return {
     get_election_surprise: function(year, area) {
       return 0;
     },
-    get_individual_surprise(year, area, surname) {
+    get_individual_surprise: function(year, area, surname) {
       return 0;
     }
   }
